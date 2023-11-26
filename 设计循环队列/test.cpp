a432db563c00cb0bab04904d2c011e95bae22fec
一只贾 <3378079760@qq.com>
2023-11-26 13:03:52 +0000

    队列的应用
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 设计循环队列/test.cpp
@@ -0,0 +1,134 @@
+﻿//设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+//
+//循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+//
+//你的实现应该支持如下操作：
+//
+//MyCircularQueue(k) : 构造器，设置队列长度为 k 。
+//Front : 从队首获取元素。如果队列为空，返回 - 1 。
+//Rear : 获取队尾元素。如果队列为空，返回 - 1 。
+//enQueue(value) : 向循环队列插入一个元素。如果成功插入则返回真。
+//deQueue() : 从循环队列中删除一个元素。如果成功删除则返回真。
+//isEmpty() : 检查循环队列是否为空。
+//isFull() : 检查循环队列是否已满。
+//
+//
+//示例：
+//
+//MyCircularQueue circularQueue = new MyCircularQueue(3); // 设置长度为 3
+//circularQueue.enQueue(1);  // 返回 true
+//circularQueue.enQueue(2);  // 返回 true
+//circularQueue.enQueue(3);  // 返回 true
+//circularQueue.enQueue(4);  // 返回 false，队列已满
+//circularQueue.Rear();  // 返回 3
+//circularQueue.isFull();  // 返回 true
+//circularQueue.deQueue();  // 返回 true
+//circularQueue.enQueue(4);  // 返回 true
+//circularQueue.Rear();  // 返回 4
+//
+//
+
+
+//力扣第622题
+typedef struct {
+    //用数组储存队列
+    int* a;
+    //队列中有多少个数据
+    int k;
+    //第一个数据的储存位置
+    int front;
+    //最后一个数据的储存位置
+    int tail;
+} MyCircularQueue;
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj);
+bool myCircularQueueIsFull(MyCircularQueue* obj);
+
+MyCircularQueue* myCircularQueueCreate(int k) {
+    MyCircularQueue* q = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+    q->a = (int*)malloc(sizeof(int) * (k + 1));
+    q->k = k;
+    q->front = q->tail = 0;
+    return q;
+}
+
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+    assert(obj);
+    if (myCircularQueueIsFull(obj)) {
+        return false;
+    }
+    obj->a[obj->tail] = value;
+    obj->tail++;
+    if ((obj->tail) > (obj->k)) {
+        obj->tail = 0;
+    }
+    return true;
+}
+
+bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+    assert(obj);
+    if (myCircularQueueIsEmpty(obj)) {
+        return false;
+    }
+    obj->front++;
+    if ((obj->front) > (obj->k)) {
+        obj->front = 0;
+    }
+    return true;
+}
+
+int myCircularQueueFront(MyCircularQueue* obj) {
+    assert(obj);
+    if (myCircularQueueIsEmpty(obj)) {
+        return -1;
+    }
+    return obj->a[obj->front];
+}
+
+int myCircularQueueRear(MyCircularQueue* obj) {
+    if (myCircularQueueIsEmpty(obj)) {
+        return -1;
+    }
+    //tail在数组第一个的情况
+    if (obj->tail == 0) {
+        return obj->a[obj->k];
+    }
+    return obj->a[obj->tail - 1];
+}
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+    if (obj->front == obj->tail) {
+        return true;
+    }
+    return false;
+}
+
+bool myCircularQueueIsFull(MyCircularQueue* obj) {
+    if ((obj->tail + 1) % (obj->k + 1) == obj->front) {
+        return true;
+    }
+    return false;
+}
+
+void myCircularQueueFree(MyCircularQueue* obj) {
+    free(obj->a);
+    free(obj);
+}
+
+/**
+ * Your MyCircularQueue struct will be instantiated and called as such:
+ * MyCircularQueue* obj = myCircularQueueCreate(k);
+ * bool param_1 = myCircularQueueEnQueue(obj, value);
+
+ * bool param_2 = myCircularQueueDeQueue(obj);
+
+ * int param_3 = myCircularQueueFront(obj);
+
+ * int param_4 = myCircularQueueRear(obj);
+
+ * bool param_5 = myCircularQueueIsEmpty(obj);
+
+ * bool param_6 = myCircularQueueIsFull(obj);
+
+ * myCircularQueueFree(obj);
+*/
