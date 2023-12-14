53274c9062f60ee21f20b562a0961d7cab187922
一只贾 <3378079760@qq.com>
2023-12-14 16:12:47 +0000

    排序
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 重复元素/test.cpp
@@ -0,0 +1,33 @@
+﻿//给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+//
+//
+//示例 1：
+//
+//输入：nums = [1, 2, 3, 1]
+//输出：true
+//示例 2：
+//
+//输入：nums = [1, 2, 3, 4]
+//输出：false
+//示例 3：
+//
+//输入：nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+//输出：true
+
+
+
+
+int cmp(const void* _a, const void* _b) {
+
+    return *(int*)_a - *(int*)_b;
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
