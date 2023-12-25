1e8a43a87d63f4f7861970ec282ee933951971ae
一只贾 <3378079760@qq.com>
2023-12-25 12:02:22 +0000

    笛卡尔函数
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 爱心代码/test.c
@@ -0,0 +1,22 @@
+﻿
+//以笛卡尔函数为基础，数学解决实际问题
+#include<stdio.h>
+#include<Windows.h>
+
+
+int main() {
+	system("color 0c");
+	float x, y;                   
+
+		for (y = 1.5f; y > -1; y -= 0.1f) { 
+			for (x = -1.5f; x < 1.5f; x +=0.05f) {
+               float a = x * x + y * y - 1;
+			     printf(a * a * a - x * x * y * y* y <0.0f ? "x" : " ");
+			}
+			Sleep(150);
+		  printf("\n");
+			
+		}
+
+	return 0;
+}
