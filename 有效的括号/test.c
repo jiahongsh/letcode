ec83b9a2300452411d3d8f95b218487207e97773
一只贾 <3378079760@qq.com>
2024-02-11 18:28:41 +0000

    模拟
    Signed-off-by: 一只贾 <3378079760@qq.com>

---
+++ 有效的括号/test.c
@@ -0,0 +1,59 @@
+﻿//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+//
+//有效字符串需满足：
+//
+//左括号必须用相同类型的右括号闭合。
+//左括号必须以正确的顺序闭合。
+//每个右括号都有一个对应的相同类型的左括号。
+//
+//
+//示例 1：
+//
+//输入：s = "()"
+//输出：true
+//示例 2：
+//
+//输入：s = "()[]{}"
+//输出：true
+//示例 3：
+//
+//输入：s = "(]"
+//输出：false
+//
+//
+//提示：
+//
+//1 <= s.length <= 104
+//s 仅由括号 '()[]{}' 组成
+
+
+
+//力扣第20题
+char pairs(char a) {
+    if (a == '}') return '{';
+    if (a == ']') return '[';
+    if (a == ')') return '(';
+    return 0;
+}
+
+bool isValid(char* s) {
+    int n = strlen(s);
+    if (n % 2 == 1) {
+        return false;
+    }
+    int stk[n + 1], top = 0;
+    for (int i = 0; i < n; i++) {
+        char ch = pairs(s[i]);
+        if (ch) {
+            if (top == 0 || stk[top - 1] != ch) {
+                return false;
+            }
+            top--;
+        }
+        else {
+            stk[top++] = s[i];
+        }
+    }
+    return top == 0;
+}
+
