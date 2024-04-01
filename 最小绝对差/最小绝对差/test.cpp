d90d8dc7bc61158a36cb8ca63a3d37a1d7b918dd
jiahongsh <152300842+jiahongsh@users.noreply.github.com>
2024-04-01 21:15:40 +0800

    Add files via upload

---
+++ 最小绝对差/最小绝对差/test.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<vector>
+#include<Algorithm>
+using namespace std;
+
+vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+    vector<vector<int>>ans;
+    int min = INT_MAX;
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n - 1; i++)
+    {
+        int num = arr[i + 1] - arr[i];
+        if (num < min)
+        {
+            min = num;
+            ans.clear();
+            ans.push_back({ arr[i],arr[i + 1] });
+        }
+        else if (min == num)
+        {
+            ans.push_back({ arr[i],arr[i + 1] });
+        }
+    }
+
+
+    return ans;
+
+}
